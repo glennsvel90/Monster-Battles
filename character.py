28086b3bf8ea47f3615f916b3e6731d11d49e1e0
glennsvel90 <glennselwynvel@gmail.com>
2018-05-09 14:16:37 -0400

    fix merge

--- character.py
+++ character.py
@@ -4,31 +4,31 @@ from combat import Combat
 import random
 
 class Character(Combat):
-<<<<<<< HEAD
+
     attack_limit = 10
     experience = 0
     base_hit_points = 10
-=======
+
     """ The user player's character """
-    
+
     attack_limit=10
     experience=0
     base_hit_points=10
-    
+
     def __init__(self,**kwarg):
         """ Ask for player's name """
-        
+
         self.name= input("What's your name: ")
         self.weapon=self.get_weapon()
         self.hit_points=self.base_hit_points
 
         for k,v in kwarg.items():
             setattr(self,k,v)
->>>>>>> 250ab5f776d0c61b9b8a203cd3fa84f77cd07891
+
 
     def attack(self):
         """ attacks a monster """
-        
+
         roll=random.randint(1,self.attack_limit)
         if self.weapon =='sword':
             roll += 1
@@ -38,7 +38,7 @@ class Character(Combat):
 
     def get_weapon(self):
         """ Makes main character choose a sword, axe, or bow """
-        
+
         weapon_choice=input('Choose a weapon: [S]word, [A]xe, [B]ow: ').lower()
 
         if weapon_choice in 'sab':
@@ -51,7 +51,6 @@ class Character(Combat):
         else:
             return self.get_weapon()
 
-<<<<<<< HEAD
     def __init__(self,**kwarg):
         self.name = input("What's your name: ")
         self.weapon = self.get_weapon()
@@ -60,28 +59,26 @@ class Character(Combat):
         for k,v in kwarg.items():
             setattr(self,k,v)
 
-=======
->>>>>>> 250ab5f776d0c61b9b8a203cd3fa84f77cd07891
+
     def __str__(self):
         """ Return the character name, HP,  and XP """
-        
+
         return '{} (HP:{}, XP:{})'.format(self.name, self.hit_points,self.experience)
 
     def rest(self):
-<<<<<<< HEAD
+
         if self.hit_points < self.base_hit_points:
             self.hit_points += 1
 
     def leveled_up(self):
         return self.experience >= 5
-=======
+
         """ Increase life hit points by 1 """
-        
+
         if self.hit_points<self.base_hit_points:
             self.hit_points+=1
 
     def leveled_up(self):
         """ Level up the main character """
-        
+
         return self.experience>=5
->>>>>>> 250ab5f776d0c61b9b8a203cd3fa84f77cd07891
